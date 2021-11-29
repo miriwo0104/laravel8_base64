84625af9d3d4bde7c335b553a66ff5fd1f309c7e
Shun Okawa <shunokawa@gmail.com>
2021-11-29 23:38:27 +0900

    エンコード デコード それぞれ最小の実装完了

---
+++ app/Http/Controllers/ConvertController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EncodeRequest;
+use App\Http\Requests\DecodeRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+
+class ConvertController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function encode(EncodeRequest $request)
+    {
+        $postData = $request->validated();
+        // ファイルを文字列とする
+        $file = file_get_contents($postData['file']);
+        // ファイルをbase64でエンコード
+        $encodedBase64Str = base64_encode($file);
+        // view表示用データの整形
+        $viewData = [
+            'encodedBase64Str' => $encodedBase64Str,
+        ];
+
+        return view('index', ['viewData' => $viewData]);
+    }
+
+    public function decode(DecodeRequest $request)
+    {
+        $postData = $request->validated();
+
+        // base64文字列の最初の部分にmime情報などが合った場合排除
+        $base64Str = str_replace(' ', '+', preg_replace('/^data:image.*base64,/', '', $postData['decodeBase64Str']));
+        // ファイルのデコード
+        $decodeFile = base64_decode($base64Str);
+        // mimeの取得
+        $mime = finfo_buffer(finfo_open(FILEINFO_MIME_TYPE), $decodeFile);
+
+        // view表示用データの整形
+        $viewData = [
+            'decodeBase64Str' => $postData['decodeBase64Str'],
+            'mime' => $mime,
+        ];
+
+        return view('index', ['viewData' => $viewData]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Covert</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="encode">
+            <p>ファイル → Base64 へエンコード</p>
+            @error('file')
+                {{ $message }}
+            @enderror
+            @if ( empty($viewData['encodedBase64Str']) )
+                <form action="{{ route('encode') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="file" id="input-file">
+                    <input type="submit" value="変換">
+                </form>
+            @else
+                <p>{{ $viewData['encodedBase64Str'] }}</p>
+                <textarea name="" id="" cols="180" rows="10">{{ $viewData['encodedBase64Str'] }}</textarea>
+            @endif
+        </div>
+        <div class="decode">
+            <p>Base64 → ファイル デコード</p>
+            @error('decodeBase64Str')
+                {{ $message }}
+            @enderror
+            @if ( empty($viewData['decodeBase64Str']) )
+                <form action="{{ route('decode') }}" method="post">
+                    @csrf
+                    <input type="text" name="decodeBase64Str" id="input-base64_str">
+                    <input type="submit" value="変換">
+                </form>
+            @else
+                <img src="data: {{ $viewData['mime'] }};base64, {{ $viewData['decodeBase64Str'] }}">
+            @endif
+        </div>
+        <div class="clear">
+            <a href="{{ route('index') }}">
+                <button>内容クリア</button>
+            </a>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+</html>
